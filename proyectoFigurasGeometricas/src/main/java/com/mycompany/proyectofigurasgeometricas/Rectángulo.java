ccb1c593d5259708c5df55427b3244cfb6cc4009
Karen Bayona <122847764+Ksren2804@users.noreply.github.com>
2024-10-20 22:56:04 -0500

    Update and rename Rectangulo.java to Rectángulo.java

--- proyectoFigurasGeometricas/src/main/java/com/mycompany/proyectofigurasgeometricas/Rectángulo.java
+++ proyectoFigurasGeometricas/src/main/java/com/mycompany/proyectofigurasgeometricas/Rectángulo.java
@@ -3,13 +3,13 @@ package com.mycompany.proyectofigurasgeometricas;
 /**
  * Complejidad temporal: O(1) Tiempo constante.
  */
-public class Rectangulo extends FiguraGeometrica{
+public class Rectángulo extends FiguraGeometrica{
    
    //atributos
     private int lado1;
     private int lado2;
 
-    public Rectangulo(String nombre, String color, int lado1, int lado2) {
+    public Rectángulo(String nombre, String color, int lado1, int lado2) {
         super(nombre, color);
         this.lado1 = lado1;
         this.lado2 = lado2;
